4261d7710d2579abfc833148e2a1df494d4cb7d7
Stephen-eze <160481441+Stephen-eze@users.noreply.github.com>
2024-02-19 11:26:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Okechukwu Stephen Eze";
+$regNo = "2022/HND/34731/CS";
+$stateOfOrigin = "Ebonyi";
+$localGovt = "Afikpo North";
+$age = 24;
+
+echo "Name: $name<br>";
+echo "Registration Number: $regNo<br>";
+echo "State of Origin: $stateOfOrigin<br>";
+echo "Local Government Area: $localGovt<br>";
+echo "Age: $age years old";
+?>
